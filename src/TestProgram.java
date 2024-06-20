c9a1efb847ae49fd1d12d9eddaada81b6b422d46
farhana akter <farhanaakter120223@gmail.com>
2024-06-20 20:30:50 +0600

    E-9.6

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -1,13 +1,10 @@
 public class TestProgram {
     public static void main(String[] args) {
-        NumericQuestion nq = new NumericQuestion();
-        nq.setText("What is 1 + 1?");
-        nq.setAnswer(2.0);
+        FillInQuestion fiq = new FillInQuestion();
+        fiq.setText("The inventor of Java was _James Gosling_");
 
-        System.out.println("Question: " + nq.display());
-        System.out.println("Response '2.0' is " + (nq.checkAnswer("2.0") ? "correct" : "incorrect"));
-        System.out.println("Response '2.01' is " + (nq.checkAnswer("2.01") ? "correct" : "incorrect"));
-        System.out.println("Response '1.98' is " + (nq.checkAnswer("1.98") ? "correct" : "incorrect"));
-        System.out.println("Response '3.0' is " + (nq.checkAnswer("3.0") ? "correct" : "incorrect"));
+        System.out.println("Question: " + fiq.display());
+        System.out.println("Response 'James Gosling' is " + (fiq.checkAnswer("James Gosling") ? "correct" : "incorrect"));
+        System.out.println("Response 'Gosling' is " + (fiq.checkAnswer("Gosling") ? "correct" : "incorrect"));
     }
 }
