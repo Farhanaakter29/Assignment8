e2ce0c79f9d3fb7c775fc10f7a40b14ef1f546f5
farhana akter <farhanaakter120223@gmail.com>
2024-11-21 15:47:35 +0600

    E-9.18

--- src/LabeledPoint.java
+++ src/LabeledPoint.java
@@ -1,18 +1,20 @@
+import java.awt.Point;
+
 public class LabeledPoint {
-    private int x;
-    private int y;
-    private String label;
+    private Point point;  // Point object to store x, y coordinates
+    private String label; // Label for the point
 
     // Constructor to initialize the point with x, y, and label
     public LabeledPoint(int x, int y, String label) {
-        this.x = x;
-        this.y = y;
-        this.label = label;
+        this.point = new Point(x, y); // Initialize the Point object
+        this.label = label;           // Set the label
     }
 
     // toString method to return a string representation of the point
     @Override
     public String toString() {
-        return "Point(" + x + ", " + y + ", Label: " + label + ")";
+        // Using the toString method of the Point class and appending the label
+        return point.toString() + ", Label: " + label;
     }
 }
+
